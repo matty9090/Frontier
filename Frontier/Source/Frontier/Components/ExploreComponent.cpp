dbc3c7c7f6590eac3e3bdbc28d9e881fb8ad665c
Matthew Lowe <lowe.matthew@mail.com>
2019-10-20 20:12:08 +0100

    Add scout

---
+++ Frontier/Source/Frontier/Components/ExploreComponent.cpp
@@ -0,0 +1,56 @@
+// Copyright Nathan Williams & Matthew Lowe 2019. All Rights Reserved.
+
+#include "ExploreComponent.h"
+#include "FrontierCharacter.h"
+#include "UnrealNetwork.h"
+#include "Frontier.h"
+
+UExploreComponent::UExploreComponent()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UExploreComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+    AIController = Cast<AAIController>(Cast<AFrontierCharacter>(GetOwner())->GetController());
+
+    if (AIController)
+    {
+        // OnMoveCompleteDelegate.BindUFunction(this, "OnMoveComplete");
+        auto PathComp = Cast<UPathFollowingComponent>(AIController->GetComponentByClass(UPathFollowingComponent::StaticClass()));
+        OnMoveCompleteDelegate = PathComp->OnRequestFinished.AddUObject(this, &UExploreComponent::OnMoveComplete);
+
+        if (GetOwner()->HasAuthority())
+        {
+            PickTargetLocation();
+            AIController->MoveToLocation(TargetLocation, AcceptanceRadius);
+        }
+    }
+}
+
+void UExploreComponent::PickTargetLocation()
+{
+    FVector CurrentLocation = GetOwner()->GetActorLocation();
+    auto RandomRotation = FRotator::MakeFromEuler(FVector(0.0f, 0.0f, FMath::RandRange(0.0f, 360.0f)));
+
+    TargetLocation = CurrentLocation + RandomRotation.RotateVector(FVector(1.0f, 0.0f, 0.0f)) * SearchRadius;
+}
+
+void UExploreComponent::OnMoveComplete(FAIRequestID RequestID, const FPathFollowingResult& Result)
+{
+    if (GetOwner()->HasAuthority())
+    {
+        PickTargetLocation();
+        AIController->MoveToLocation(TargetLocation, AcceptanceRadius);
+    }
+}
+
+void UExploreComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(UExploreComponent, TargetLocation);
+}
---
+++ Frontier/Source/Frontier/Components/ExploreComponent.h
@@ -0,0 +1,40 @@
+// Copyright Nathan Williams & Matthew Lowe 2019. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Classes/AIController.h"
+#include "ExploreComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class FRONTIER_API UExploreComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UExploreComponent();
+
+    void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+    UPROPERTY(EditAnywhere)
+    float SearchRadius = 2600.0f;
+
+    UPROPERTY(EditAnywhere)
+    float AcceptanceRadius = 200.0f;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+    void PickTargetLocation();
+    void OnMoveComplete(FAIRequestID RequestID, const FPathFollowingResult& Result);
+
+    bool bExploring = true;
+    AAIController* AIController;
+    FDelegateHandle OnMoveCompleteDelegate;
+
+    UPROPERTY(Replicated)
+    FVector TargetLocation;
+};
